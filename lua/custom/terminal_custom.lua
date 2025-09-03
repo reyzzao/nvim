732bbbfaa94833e01da891d520cd14afd5d925c8
reizzao <reizao1977@gmail.com>
2025-09-03 10:49:06 -0300

    reinicio clean main

--- lua/custom/terminal_custom.lua
+++
@@ -1,9 +0,0 @@
--- @file: ~/.config/nvim/lua/custom/terminal_custom.lua 
-
--- abaixo para funcionar o ctr+click no terminal --
-vim.api.nvim_set_option('path', vim.fn.substitute(vim.fn.getcwd(), '/', '\\/', 'g')) -- terminal mostrar o endereco dclicavel certoe erro cli
-vim.api.nvim_set_option('tags', '~/dotfiles/nvim/.config/nvim/tags/tags') -- Define o caminho para o ARQUIVO de tags - para abrir links clicaveis no terminal
-
--- configura o Neovim para abrir o arquivo correto quando você clica com o lado direito do mouse
-vim.api.nvim_set_keymap('n', '<C-Click>', ':e <cfile><CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<RightMouse>', ':e <cfile><CR>', { noremap = true, silent = true })
--- lua/keymaps/explorer.lua
+++
@@ -1,9 +0,0 @@
--- @file: ~/.config/nvim/lua/keymaps/explorer.lua
-
-return {}
-
--- Navegacao do Neo-tree
--- Subir um nivel
--- vim.keymap.set('n', '..', ":lua require('neo-tree.sources.filesystem').change_root('../')<CR>", { desc = "Subir um nivel no diretorio" })
--- -- Voltar para a raiz do projeto
--- vim.keymap.set('n', '<leader>pr', ":lua require('neo-tree.sources.filesystem').change_root(vim.fn.getcwd())<CR>", { desc = "Voltar a raiz do projeto" })
